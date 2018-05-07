f93d02ab469b07debadb2ca9aacd778a6b7b18d9
chenjingduo <chenjingduo@corp.netease.com>
2018-05-07 17:36:32 +0800

    native_call, inner_action, native-call direct fliter

--- React/Views/RCTWebViewManager.m
+++ React/Views/RCTWebViewManager.m
@@ -118,7 +118,7 @@ shouldStartLoadForRequest:(NSMutableDictionary<NSString *, id> *)request
   } else {
     RCTLogWarn(@"Did not receive response to shouldStartLoad in time, defaulting to YES");
     NSString *url = request[@"url"];
-    if ([url rangeOfString:@"native_call"].location != NSNotFound) {
+    if ([url rangeOfString:@"native_call"].location != NSNotFound || [url rangeOfString:@"inner_action"].location != NSNotFound || [url rangeOfString:@"native-call"].location != NSNotFound ) {
         return NO;
     }
     return YES;
