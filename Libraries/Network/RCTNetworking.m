59e00b1846865fd965309744205e9fa3a34c6016
Lisa <lijing01@corp.netease.com>
2019-05-17 16:59:09 +0800

    pass error code to rn

--- Libraries/Network/RCTNetworking.m
+++ Libraries/Network/RCTNetworking.m
@@ -405,7 +405,8 @@ RCT_EXPORT_MODULE()
       }
       NSArray *responseJSON = @[task.requestID,
                                 RCTNullIfNil(error.localizedDescription),
-                                error.code == kCFURLErrorTimedOut ? @YES : @NO
+                                error.code == kCFURLErrorTimedOut ? @YES : @NO,
+                                @(error.code)
                                 ];
 
       [self sendEventWithName:@"didCompleteNetworkResponse" body:responseJSON];
