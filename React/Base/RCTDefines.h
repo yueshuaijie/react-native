002ecba136c70371d978448f395d7e217b96ae79
gzwuguanghong <gzwuguanghong@corp.netease.com>
2019-08-20 19:58:49 +0800

    add webkit APIs available judge

--- React/Base/RCTDefines.h
+++ React/Base/RCTDefines.h
@@ -79,3 +79,14 @@ _Pragma("clang diagnostic ignored \"-Wunused-parameter\"") \
 RCT_EXTERN NSException *_RCTNotImplementedException(SEL, Class); \
 method NS_UNAVAILABLE { @throw _RCTNotImplementedException(_cmd, [self class]); } \
 _Pragma("clang diagnostic pop")
+
+
+/**
+ * Check if WebKit iOS 9.0 APIs are available.
+ */
+#define WEBKIT_IOS_9_APIS_AVAILABLE __has_include(<WebKit/WKWebsiteDataStore.h>)
+
+/**
+ * Check if WebKit iOS 10.0 APIs are available.
+ */
+#define WEBKIT_IOS_10_APIS_AVAILABLE __has_include(<WebKit/WKDataDetectorTypes.h>)
