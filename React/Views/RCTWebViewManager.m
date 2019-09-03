8f90c5c59c012f2704b5f1c55cdd7ef124ee5f13
chenjingduo <cc@jwill111.local>
2019-09-03 12:55:30 +0800

    add cc-ds-ios judge and gen tarball

--- React/Views/RCTWebViewManager.m
+++ React/Views/RCTWebViewManager.m
@@ -174,7 +174,7 @@ shouldStartLoadForRequest:(NSMutableDictionary<NSString *, id> *)request
     RCTLogWarn(@"Did not receive response to shouldStartLoad in time, defaulting to YES");
     NSString *url = request[@"url"];
 
-    if ([url rangeOfString:@"native_call"].location != NSNotFound || [url rangeOfString:@"inner_action"].location != NSNotFound || [url rangeOfString:@"inner-action"].location != NSNotFound) {
+    if ([url rangeOfString:@"native_call"].location != NSNotFound || [url rangeOfString:@"inner_action"].location != NSNotFound || [url rangeOfString:@"inner-action"].location != NSNotFound  || [url rangeOfString:@"cc-ds-ios"].location != NSNotFound) {
       return NO;
     }
     return YES;
